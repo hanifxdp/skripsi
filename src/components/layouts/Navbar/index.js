0b5f84e3baf50c5fa2458f017400c7a493bab5ac
hanifxdp <briandglory@gmail.com>
2022-08-11 18:45:57 +0700

    edit logo

--- src/components/layouts/Navbar/index.js
+++ src/components/layouts/Navbar/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Transition } from "@headlessui/react";
 import MenuDisclosure from "../MenuDisclosure";
 import { budayaLinks, provinsiLinks } from "../../constant/Disclosured";
+import logo from "../../../assets/img/icon.png";
 
 const Navbar = (props) => {
 	const { show } = props;
@@ -17,10 +18,13 @@ const Navbar = (props) => {
 			leaveTo="-translate-x-full"
 		>
 			<div className="sticky top-0 left-0 h-screen space-y-2 overflow-y-auto bg-white shadow-xl w-80 no-scrollbar">
-				<div className="px-12 py-8">
-					<h5 className="text-xl font-bold">
-						Manajemen Data Kerajinan Tradisional Indonesia
-					</h5>
+				<div className="px-6 py-6 flex justify-items-center space-x-2">
+					<img src={logo} className=" w-30 h-20 " alt="" />
+					<div className="flex flex-col justify-center">
+						<h5 className="text-lg font-bold">
+							Manajemen Data Kerajinan Tradisional Indonesia
+						</h5>
+					</div>
 				</div>
 				<div className="space-y-0.5 px-1">
 					<MenuDisclosure links={budayaLinks} />
